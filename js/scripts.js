b0b1acea021125971e356fc1963f56a03640f7db
jrickard <joerickard@hotmail.co.uk>
2019-06-14 08:56:39 +0100

    Added comments #6

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 function openPage(pageName, iconid) {
-  // Hide all elements with class="tabcontent" by default */
+  // Hide all elements with class="tabcontent" by default
   var i, tabcontent, icons, activeIcon;
   tabcontent = document.getElementsByClassName("tabcontent");
   for (i = 0; i < tabcontent.length; i++) {
@@ -8,12 +8,13 @@ function openPage(pageName, iconid) {
   // Show the specific tab content
   document.getElementById(pageName).style.display = "block";
   
+  // Sets inactive icons style back to default (normal shadow)
   icons = document.getElementsByClassName("roundIcon");
   for (i = 0; i < icons.length; i++) {
     icons[i].setAttribute("style","box-shadow: 1px 1px 8px grey")
   }
   
-  //Set style for active icon
+  // Set style for active icon (adds a darker shadow)
   activeIcon = document.getElementById(iconid);
   activeIcon.setAttribute("style","box-shadow: 1px 1px 15px black")
 }
